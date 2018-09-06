1ddcabb3a6ba6e8b4f9219dc6f985953dffc43b1
z1176 <11764404@qq.com>
2018-09-06 23:39:59 +0800

    更新手机端轮播接口

--- shareskills/src/main/java/com/shareskill/controller/HomeController.java
+++ shareskills/src/main/java/com/shareskill/controller/HomeController.java
@@ -4,11 +4,12 @@ import org.springframework.ui.Model;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.Map;
 
 public interface HomeController {
     /**映射首页请求*/
 
     public String showHome(HttpServletRequest request,Model model,Integer pageNo);
     /**手机端返回轮播图片地址*/
-    public List<Object> showPicForPhone(HttpServletRequest request, Model model);
+    public Map showPicForPhone(HttpServletRequest request, Model model);
 }
